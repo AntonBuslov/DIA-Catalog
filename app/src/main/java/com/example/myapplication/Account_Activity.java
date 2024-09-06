8b48cae0f35fd757d44bcab29f271c4b6f4a7926
ivan <aakarpenko2013@gmail.com>
2024-09-06 18:24:44 +0300

    Shoving registered name and email in profile

--- app/src/main/java/com/example/myapplication/Account_Activity.java
+++ app/src/main/java/com/example/myapplication/Account_Activity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -11,18 +14,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Account_Activity extends AppCompatActivity {
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
 
     BottomNavigationView navigationView;
+
+    TextView emailText, usernameText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_account);
         navigationView = findViewById(R.id.navigationBar);
+
+        emailText = findViewById(R.id.emailText);
+        usernameText = findViewById(R.id.usernameText);
+
+
         navigationView.setSelectedItemId(R.id.profile);
 
         navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -50,5 +69,26 @@ public class Account_Activity extends AppCompatActivity {
                 return false;
             }
         });
+        DocumentReference docRef = firebaseFirestore.collection("users").document(firebaseAuth.getCurrentUser().getUid());
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot doc = task.getResult();
+                    if(doc.exists()){
+                        emailText.setText(doc.get("Email").toString());
+                        usernameText.setText(doc.get("Name").toString());
+                    }
+                }
+            }
+        });
+
+    }
+
+    public void LogOut(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(Account_Activity.this, MainActivity.class));
+        finish();
     }
 }
