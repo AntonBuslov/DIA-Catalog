c0ca09d627d7bf59365fd42526ba3a7a9d0e67ce
Denis Tikholaz <tikholaz.fak@gmail.com>
2024-09-11 23:34:45 +0300

    product

--- app/src/main/java/com/example/myapplication/RecommendedAdapt.kt
+++ app/src/main/java/com/example/myapplication/RecommendedAdapt.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.ViewRecommendedBinding
@@ -24,8 +27,13 @@ return Viewholder(binding)
             Glide.with(holder.itemView.context)
                 .load(item.picUrl[0])
                 .into(pic)
-            root.setOnClickListener{
 
+
+            root.setOnClickListener{
+val intent= Intent(holder.itemView.context,DetailAktivity::class.java).apply{
+    putExtra("object",item)
+}
+            ContextCompat.startActivity(holder.itemView.context,intent,null)
             }
         }
     }
